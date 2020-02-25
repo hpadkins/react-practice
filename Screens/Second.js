33e177f24506490f4eec674f0b0b3f21147bd612
ernes7 <ernes@pdx.edu>
2020-02-24 19:50:36 -0800

    2 questions

--- Screens/Second.js
+++ Screens/Second.js
@@ -1,14 +1,29 @@
 import React, { Component }from 'react';
-import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, Alert} from 'react-native';
 
 export default class Second extends Component {
 
 
 
-  constructor(props: Props) {
+  constructor(props) {
     super(props);
     this.state = {answers: {one: "", two: ""}};
+  }
 
+  reed = () => {
+    //console.log(this.state.answers.one + " " +this.state.answers.two)
+    if(this.state.answers.one == "Harriet" && this.state.answers.two == "Hummel")
+    {
+
+      alert(
+        'Alert Title',
+        'My Alert msg'
+      )
+    }
+    else{
+      alert("Wrong!!!!")
+      this.setState({answers: {one: "", two: ""}});
+    }
   }
     
   render() {
@@ -19,28 +34,31 @@ export default class Second extends Component {
         <Text style={{padding: 10}}> What is the Capital of Oregon?</Text>
         <TextInput 
           style={{ height: 40, width: 100, borderColor: 'gray', borderWidth: 1 }}
-          onChangeText={(answer) => this.setState({answers:{one:answer}})}
+          onChangeText={(answer) => this.setState({answers:{one:answer, two:this.state.answers.two}})}
           value={this.state.answers.one}
         />
         
-        {/* <Text style={{padding: 10}}>
+  {/*       <Text style={{padding: 10}}>
           You entered: {this.state.answers.one}
-        </Text> */}
-        <TouchableOpacity style ={{
-          backgroundColor: '#7a42f4',
-          padding: 10,
-          margin: 15,
-          height: 40,
-        }}>
-          <Text> Submit </Text>
-        </TouchableOpacity>
-       {/* <CheckBox
-        title = "test"
-        checked={this.state.checked}
-        /> */}
+        </Text>  */}
+
+        <Text style={{padding: 10}}> What is reed's last name?</Text>
+        <TextInput 
+          style={{ height: 40, width: 100, marginBottom: 25, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(answer) => this.setState({answers:{one:this.state.answers.one,two:answer}})}
+          value={this.state.answers.two}
+        />
+        
+ {/*        <Text style={{padding: 10}}>
+          You entered: {this.state.answers.two}
+        </Text> 
+ */}
+
         <Button
-          title="Finish!"
-          onPress={() => this.props.navigation.navigate('Third')}
+          title = "Finish"
+          style = {{padding: 10}}
+          //onPress={() => this.props.navigation.navigate('Third')}
+          onPress={() => this.reed()}
         />
       </View>
     );
