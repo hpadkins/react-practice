7a54c7f2a25cd9a535257f1628bbd211f89a5f36
hpadkins <hadkins@pdx.edu>
2020-02-24 18:53:25 -0800

    textinput quiz

--- Screens/Second.js
+++ Screens/Second.js
@@ -1,11 +1,43 @@
 import React, { Component }from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity} from 'react-native';
 
 export default class Second extends Component {
+
+
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {answers: {one: "", two: ""}};
+
+  }
+    
   render() {
     return (
       <View style={styles.container}>
-        <Text>Second Screen</Text>
+
+  
+        <Text style={{padding: 10}}> What is the Capital of Oregon?</Text>
+        <TextInput 
+          style={{ height: 40, width: 100, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(answer) => this.setState({answers:{one:answer}})}
+          value={this.state.answers.one}
+        />
+        
+        {/* <Text style={{padding: 10}}>
+          You entered: {this.state.answers.one}
+        </Text> */}
+        <TouchableOpacity style ={{
+          backgroundColor: '#7a42f4',
+          padding: 10,
+          margin: 15,
+          height: 40,
+        }}>
+          <Text> Submit </Text>
+        </TouchableOpacity>
+       {/* <CheckBox
+        title = "test"
+        checked={this.state.checked}
+        /> */}
         <Button
           title="Finish!"
           onPress={() => this.props.navigation.navigate('Third')}
